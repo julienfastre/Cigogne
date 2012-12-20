42bbb6f82ea730d2b43245736e49dff6e2265b2f
Julien Fastré <julien@fastre.info>
2012-12-20 23:21:31 +0100

    Initial commit

---
+++ ui/Cigogne/src/org/fastre/cigogne/client/CigogneEntryPoint.java
@@ -0,0 +1,13 @@
+package org.fastre.cigogne.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class CigogneEntryPoint implements EntryPoint {
+
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		Window.alert("hello ça marche");
+	}
+
+}
