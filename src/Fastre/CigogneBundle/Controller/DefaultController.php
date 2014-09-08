bf076386c1be20d0bc87a938cf9c0d7ad6470a74
Julien Fastré <julien@fastre.info>
2014-09-08 15:24:37 +0200

    persona-explain

--- src/Fastre/CigogneBundle/Controller/DefaultController.php
+++ src/Fastre/CigogneBundle/Controller/DefaultController.php
@@ -3,11 +3,21 @@
 namespace Fastre\CigogneBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use InvalidArgumentException;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('FastreCigogneBundle:Default:index.html.twig');
+    }
+    
+    public function pageAction($page)
+    {
+       try {
+          return $this->render('FastreCigogneBundle:Page:'.$page.'.html.twig');
+       } catch (InvalidArgumentException $e) {
+          throw $this->createNotFoundException('page not found');
+       }
     }
 }
